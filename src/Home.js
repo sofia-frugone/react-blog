43794183cb1f88d0a1aa57e7bbf49e65d287da30
sofia_frugone <sofiabellaf2000@gmail.com>
2022-08-31 14:06:09 +1000

    updated hooks and added db.json file

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,44 @@ import { useState, useEffect } from "react";
 import BlogList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const [name, setName] = useState('mario');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs);
-  }
-
+// Use effect hook: fetching the data
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      // First we get the response object and use the JSON method
+      .then(res => {
+        if(!res.ok) {
+          // when we throw an error it is caught in the catch error block
+          throw Error('could not fetch the data for that resource')
+        }
+        return res.json();
+      })
+      // Then we use another then method where we get the data
+      .then(data => {
+        // taking the array of objects and updating the blog state with that array
+        setBlogs(data);
+        // as soon as the data appears, we set isPending to false and remove the loading message
+        setIsPending(false);
+        setError(null);
+      })
+      // catch block catches any netwrok error and fires a function 
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
+    // empty dependency array only fires the function once on the initial render "[]"
+  }, [])
 
+  // Note: to use javascript inside the template we have to use curly braces
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{ name }</p>
+      { error && <div>{ error }</div>}
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
