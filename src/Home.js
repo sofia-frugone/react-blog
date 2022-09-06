6388998403e2f18a2648093ee90be6bbdfc36292
sofia_frugone <sofiabellaf2000@gmail.com>
2022-09-06 15:55:10 +1000

    added a custom hook for usefetch

--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,26 @@
-import { useState, useEffect } from "react";
-import BlogList from './BlogList'
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-
-// Use effect hook: fetching the data
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      // First we get the response object and use the JSON method
-      .then(res => {
-        if(!res.ok) {
-          // when we throw an error it is caught in the catch error block
-          throw Error('could not fetch the data for that resource')
-        }
-        return res.json();
-      })
-      // Then we use another then method where we get the data
-      .then(data => {
-        // taking the array of objects and updating the blog state with that array
-        setBlogs(data);
-        // as soon as the data appears, we set isPending to false and remove the loading message
-        setIsPending(false);
-        setError(null);
-      })
-      // catch block catches any netwrok error and fires a function 
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-    // empty dependency array only fires the function once on the initial render "[]"
-  }, [])
+  // here we destructure the three properties we get back (data, isPending, error)- i am confused on destructuring 
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs') // we need to pass through the endpoint because of the URL property
+  
 
   // Note: to use javascript inside the template we have to use curly braces
   return (
     <div className="home">
-      { error && <div>{ error }</div>}
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} /> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
 
 export default Home;
+
+// npx json-server --watch data/db.json --port 8000 command explanation 
+// json-server: runs the json server
+// --watch : flag to watch the file 
+// data/db.json : path 
+// --port 8000 : another flag for speciying the port number, it will automatically go to port 3000 if it's not specififed 
---
+++ src/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+// custom hooks in react need to start with "use"
+// we pass the "url" as a dependency 
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+// useEffect hook: fetching the data
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+      // here we get the response object and use the JSON method
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      //here we use a then method to get the data
+      .then(data => {
+        //taking the array of objects and updating the blog state with that array
+        setData(data);
+        // as soon as the data appears we set isPending to false and remove the loading message
+        setIsPending(false);
+        setError(null);
+      })
+      // catch block catches any network error and fires a function
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [url])
+
+  return { data, isPending, error };
+}
+ 
+export default useFetch;
