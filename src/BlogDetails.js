96369fcb99e901e3684cf7164f54e139867d9d62
sofia_frugone <sofiabellaf2000@gmail.com>
2022-09-07 18:01:51 +1000

    added routes for blog details page and styling

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,27 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
 const BlogDetails = () => {
+// allows us to wrap parameters from the route: still unsure on this
+// useParams hook grabs the blog id
+    const { id } = useParams();
+// here we re-use the usefetch hook to grab the three properties
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+
     return (
         <div className="blog-details">
-          <h2>Blog Details</h2>
+          { isPending && <div>Loading...</div> }
+
+          { error && <div>{ error } </div> }
+
+          { blog && (
+            <article>
+                <h2>{ blog.title }</h2>
+                <p>Written by { blog.author }</p>
+                <div>{ blog.body }</div>
+            </article>
+          )}
+
         </div>
     );
 }
